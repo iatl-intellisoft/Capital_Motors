b3f49381d71a4f1d14804e24bb77847878189a70
IATL <afra.hassan@iatl-intellisoft.com>
2023-03-07 14:19:44 +0200

    Add

--- ii_simple_check_management/__manifest__.py
+++ ii_simple_check_management/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Check Management",
+    'name': "Simple Check Management",
 
     'summary': """
         Simple Check Management""",
@@ -8,22 +8,12 @@
     'description': """
         Simple Check Management
     """,
-    'author': "Iatl-Intellisoft",
+    'author': "Iatl-intellisoft",
     'category': 'Accounting',
     'version': '0.1',
     'depends': ['account'],
     'data': [
-
-        'security/check_security.xml',
-        'security/ir.model.access.csv',
-        'data/check_payment_method.xml',
-        'wizard/check_replacement_wizard.xml',
-        'wizard/print_check_wizard.xml',
-        'views/check_view.xml',
-        'views/payment.xml',
-        # Check print report
-        'report/check_bank_template.xml',
-        'report/reports.xml',
-        'wizard/print_check_wizard.xml',
+        #'views/payment.xml',
     ],
+    'license': 'LGPL-3',
 }
--- ii_simple_check_management/models/__init__.py
+++ ii_simple_check_management/models/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from . import payment
-from . import check
-from . import money_to_text_en
-from . import money_to_text_ar
