7d3bcc4c82e2736d79ee0b3dd4dec14fa6d7e57d
IATL <afra.hassan@iatl-intellisoft.com>
2023-03-07 14:17:43 +0200

    Add

--- ii_simple_check_management/__manifest__.py
+++ ii_simple_check_management/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Simple Check Management",
+    'name': "Check Management",
 
     'summary': """
         Simple Check Management""",
@@ -8,12 +8,22 @@
     'description': """
         Simple Check Management
     """,
-    'author': "Iatl-intellisoft",
+    'author': "Iatl-Intellisoft",
     'category': 'Accounting',
     'version': '0.1',
     'depends': ['account'],
     'data': [
+
+        'security/check_security.xml',
+        'security/ir.model.access.csv',
+        'data/check_payment_method.xml',
+        'wizard/check_replacement_wizard.xml',
+        'wizard/print_check_wizard.xml',
+        'views/check_view.xml',
         'views/payment.xml',
+        # Check print report
+        'report/check_bank_template.xml',
+        'report/reports.xml',
+        'wizard/print_check_wizard.xml',
     ],
-    'license': 'LGPL-3',
 }
